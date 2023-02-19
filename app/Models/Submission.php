117eda2613da4ac9c14bc733a76fa237b074e4cc
Francisco Borquez <fborquez@outlook.com>
2023-02-19 14:01:51 -0300

    dynamo db model

--- app/Models/Submission.php
+++ app/Models/Submission.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Kitar\Dynamodb\Model\Model;
 
 class Submission extends Model
 {
